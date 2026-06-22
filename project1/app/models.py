ec02cc9343d1124e11c6f3f3b3b522db0d3b7cd1
abhiram <abhiramgopan06@gmail.com>
2026-06-22 13:41:44 +0530

    crude

--- project1/app/models.py
+++ project1/app/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+class Course(models.Model):
+    name = models.CharField(max_length=25)
+    def _str_(self):
+        return self.name
+
 class Student(models.Model):
     name = models.CharField(max_length=25)
-    course = models.CharField(max_length=20)
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
     sid = models.CharField(unique=True)
 
     def _str_(self):
--- project1/app/views.py
+++ project1/app/views.py
@@ -1,17 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Student
-# Create your views here.
-# students_list = [
-    # {"name": "Alan", "course": "BSC", "sid": 0},
-    # {"name": "Kavin", "course": "MSC", "sid": 1},
-    # {"name": "Leivin", "course": "BCA", "sid": 2},
-    # {"name": "Jaison", "course": "BTECH", "sid": 3},
-    # {"name": "Abin", "course": "MCA", "sid": 4},
-    # {"name": "Sam", "course": "MBA", "sid": 5},
-    # {"name": "Tom", "course": "MA", "sid": 6},
-    # {"name": "Jithu", "course": "BSC", "sid": 7}
-# ]
+from .models import Student,Course
+
 def home(request):
     return render(request, 'index.html')
 
@@ -23,38 +13,26 @@ def about(request):
 
 def students(request):
     students_data = Student.objects.all()
-    print(students_data)
-    return render(request, 'pages/students1.html',{'students':students_data})
+    courses = Course.objects.all()
+    if request.method == 'POST':
+        course_id = request.POST['course']
+        if course_id:
+            course = Course.objects.get(id=course_id)
+            students_data = Student.objects.filter(course = course)
+    return render(request, 'pages/students1.html',{'students':students_data,'courses':courses})
 
-# def students(request):
-#     return HttpResponse('Students')
 
-# def get_student(request,sid):
-#     print(sid)
-#     return HttpResponse(f"Name : {students_list[sid]['name']['course']}")
-    # return HttpResponse(f"Student : {students_list[id]}")
-def get_student(request,sid):
+def get_student(request,id):
     student = Student.objects.get(id=id)
     return render(request,'pages/student.html',{"student":student})
-    # print(id)
-    # print(students_list[id]['course'])
-    # print(students_list[id]['sid'])
-    # student =""
-    # for x in students_list:
-    #     if x['sid'] == sid:
-    #         student = x
-    # return render(request,'pages/student.html',{"student":student})
+   
 
 def add_student(request):
     if request.method == "POST":
         name = request.POST['name']
         course = request.POST['course']
         sid = request.POST['sid']
-        # students_list.append({
-        #     "name":name,
-        #     "course":course,
-        #     "sid":sid
-        # })
+       
         student = Student.objects.create(name=name,course=course,sid=sid)
         student.save()
         return redirect("students")
@@ -70,23 +48,12 @@ def edit_student(request,sid):
         student.course=course
         student.sid=sid
         student.save()
-        # for x in students_list:
-        #     if x['sid'] == sid:
-        #         x['name'] = name
-        #         x['course'] = course
-    #     return redirect("students")
-    # student =""
-    # for x in students_list:
-    #     if x['sid'] == sid:
-    #         student = x
-
+       
     return render(request,'pages/edit_student.html',{'student':student})
 
 
 def delete_student(request, sid):
-    # for x in students_list:
-    #     if x['sid'] == sid:
-    #         students_list.remove(x)
+   
     student = Student.objects.get(id=id)
     student.delete()
     return redirect("students")
